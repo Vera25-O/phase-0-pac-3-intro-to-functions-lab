686852db03aef258b2d1f392cfe25f82bc039c74
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-01 13:06:20 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(hello){
+    return 'HELLO';
+}
+function whisper(HELLO){
+    return 'hello';
+}
+function logWhisper(HELLO){
+    console.log("hello");
+}
+function logShout(hello){
+    console.log("HELLO");
+}
+function sayHiToHeadphonedRoommate(a){
+     if(a==a.toLowerCase())
+    {return "I can't hear you!";}
+    else if (a==a.toUpperCase())
+   { return "YES INDEED!";}
+    else if(a="Let\'s have dinner together!")
+  {return  "I would love to!";}
+}
+
